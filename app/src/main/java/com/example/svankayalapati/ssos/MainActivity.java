6b0edfc3c87292d41cf2c7bafaf808ef9998922e
Sairman Vankayalapati <svankayalapati@insulet.com>
2018-08-30 21:37:36 -0400

    AddedPrototypeImagesForHomeScreen

--- app/src/main/java/com/example/svankayalapati/ssos/MainActivity.java
+++ app/src/main/java/com/example/svankayalapati/ssos/MainActivity.java
@@ -1,13 +1,73 @@
 package com.example.svankayalapati.ssos;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    GridView androidGridView;
+
+    Integer[] imageIDs = {
+            R.drawable.controlyourhome, R.drawable.keepintouch, R.drawable.logoentertainment,
+            R.drawable.maps, R.drawable.memoriestoshare, R.drawable.todo,
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        androidGridView = (GridView) findViewById(R.id.GridView1);
+        androidGridView.setAdapter(new ImageAdapterGridView(this));
+
+        androidGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent,
+                                    View v, int position, long id) {
+                Toast.makeText(getBaseContext(), "Grid Item " + (position + 1) + " Selected", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+    public class ImageAdapterGridView extends BaseAdapter {
+        private Context mContext;
+
+        public ImageAdapterGridView(Context c) {
+            mContext = c;
+        }
+
+        public int getCount() {
+            return imageIDs.length;
+        }
+
+        public Object getItem(int position) {
+            return null;
+        }
+
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView mImageView;
+
+            if (convertView == null) {
+                mImageView = new ImageView(mContext);
+                mImageView.setLayoutParams(new GridView.LayoutParams(130, 130));
+                mImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                mImageView.setPadding(16, 16, 16, 16);
+            } else {
+                mImageView = (ImageView) convertView;
+            }
+            mImageView.setImageResource(imageIDs[position]);
+            return mImageView;
+        }
     }
 }
