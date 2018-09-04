3f92170045964855103e32028e41475d33d8bc74
sairam9999 <vsairamanroid@gmail.com>
2018-09-03 22:15:27 -0400

    Added Right Pane

--- app/src/main/java/com/example/svankayalapati/ssos/MainActivity.java
+++ app/src/main/java/com/example/svankayalapati/ssos/MainActivity.java
@@ -3,28 +3,42 @@ package com.example.svankayalapati.ssos;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     GridView androidGridView;
-
-
+    Button place_order;
+    LinearLayout place_order_layout;
     Integer[] imageIDs = {
             R.drawable.grocery, R.drawable.stationary, R.drawable.electricals,
-            R.drawable.medicines, R.drawable.services, R.drawable.smarthome,
+            R.drawable.medicines, R.drawable.smarthome, R.drawable.smarthome,
     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        place_order = (Button) findViewById(R.id.place_order);
+        place_order_layout = (LinearLayout) findViewById(R.id.place_order_layout);
+
+        place_order.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                place_order_layout.setVisibility(View.VISIBLE);
+            }
+        });
+
       //  androidGridView = (GridView) findViewById(R.id.GridView1);
         //androidGridView.setAdapter(new ImageAdapterGridView(this));
 
