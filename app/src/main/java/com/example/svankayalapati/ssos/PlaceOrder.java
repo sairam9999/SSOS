b950e8337463d244867dc968ac8c6689de559ebb
sairam9999 <vsairamanroid@gmail.com>
2018-09-12 23:00:26 -0400

    Added the proto type hash map fro the DB

--- app/src/main/java/com/example/svankayalapati/ssos/PlaceOrder.java
+++ app/src/main/java/com/example/svankayalapati/ssos/PlaceOrder.java
@@ -6,18 +6,27 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+
+import com.example.svankayalapati.ssos.db.ItemDetails;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class PlaceOrder extends AppCompatActivity {
+ItemDetails itemDetails = new ItemDetails();
+
+Button rightpane_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_place_order);
+        itemDetails.init();
+
+        rightpane_button = (Button) findViewById(R.id.rightpane_button);
 
         final ListView listview = (ListView) findViewById(R.id.place_order_listview);
         String[] values = new String[]{"Butter", "Cane Sugar", "Cheese", "Eggs", "Milk",
@@ -38,6 +47,10 @@ public class PlaceOrder extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, final View view,
                                     int position, long id) {
                 final String item = (String) parent.getItemAtPosition(position);
+
+
+               String var = itemDetails.getItemMap().get(item);
+                rightpane_button.setText(var);
                }
 
         });
