1a30fe548fb246150d5b528f43a31987bc1664c6
sairam9999 <vsairamanroid@gmail.com>
2018-09-15 22:59:13 -0400

    Calling the Description 1 & 2

--- app/src/main/java/com/example/svankayalapati/ssos/db/ItemDetails.java
+++ app/src/main/java/com/example/svankayalapati/ssos/db/ItemDetails.java
@@ -6,28 +6,64 @@ import java.util.HashMap;
 
 public class ItemDetails {
 
-HashMap <String, Item> itemMap = new HashMap<>();
+    HashMap<String, Item> itemMap = new HashMap<>();
 
-public void init(){
-    itemMap.put ("Butter", new Item("Carrington", 250, "2lb", ContainerType.JAR,"Butter", PlaceOrder.imageId_butter));
-    itemMap.put ("Cane Sugar", new Item("Florida Crystals", 250, "2lb", ContainerType.JAR,"Cane Sugar", PlaceOrder.imageId_cane_sugar));
-    itemMap.put ("Cheese", new Item("Velvetta", 250, "2lb", ContainerType.JAR,"Cheese", PlaceOrder.imageId_cheese));
-    itemMap.put ("Eggs", new Item("Roland", 250, "2lb", ContainerType.JAR,"Eggs", PlaceOrder.imageId_eggs));
-//    itemMap.put ("Milk", new Item("Kirkland", 250, "2lb", ContainerType.JAR,"Milk"));
-//    itemMap.put ("Nescafe", new Item("Nescafe", 250, "2lb", ContainerType.JAR,"Nescafe"));
-//    itemMap.put ("Oil", new Item("Wesson", 250, "2lb", ContainerType.JAR,"Butter"));
-//    itemMap.put ("Pasta", new Item("Barilla", 250, "2lb", ContainerType.JAR,"Butter"));
-//    itemMap.put ("Rice", new Item("Royal", 250, "2lb", ContainerType.JAR,"Butter"));
-//    itemMap.put ("Salt", new Item("Morton", 250, "2lb", ContainerType.JAR,"Butter"));
-//    itemMap.put ("Sugar", new Item("Splenda", 250, "2lb", ContainerType.JAR,"Butter"));
-//    itemMap.put ("Wheat Bread", new Item("Nature's Own", 250, "2lb", ContainerType.JAR,"Butter"));
-}
+    public void init() {
+        itemMap.put("Butter", new Item("Carrington", 250, "2lb",
+                ContainerType.JAR, "Butter", PlaceOrder.imageId_butter,
+                "Description 1", "Description 2"));
 
-public HashMap <String, Item> getItemMap(){
+        itemMap.put("Cane Sugar", new Item("Florida Crystals", 250, "2lb",
+                ContainerType.JAR, "Cane Sugar", PlaceOrder.imageId_cane_sugar,
+                "Description 1", "Description 2"));
 
-    return itemMap;
+        itemMap.put("Cheese", new Item("Velvetta", 250, "2lb",
+                ContainerType.JAR, "Cheese", PlaceOrder.imageId_cheese,
+                "Description 1", "Description 2"));
 
-}
+        itemMap.put("Eggs", new Item("Roland", 250, "2lb",
+                ContainerType.JAR, "Eggs", PlaceOrder.imageId_eggs,
+                "Description 1", "Description 2"));
+
+        itemMap.put("Milk", new Item("Carrington", 250, "2lb",
+                ContainerType.JAR, "Butter", PlaceOrder.imageId_butter,
+                "Description 1", "Description 2"));
+
+        itemMap.put("Nescafe", new Item("Florida Crystals", 250, "2lb",
+                ContainerType.JAR, "Cane Sugar", PlaceOrder.imageId_cane_sugar,
+                "Description 1", "Description 2"));
+
+        itemMap.put("Oil", new Item("Velvetta", 250, "2lb",
+                ContainerType.JAR, "Cheese", PlaceOrder.imageId_cheese,
+                "Description 1", "Description 2"));
+
+        itemMap.put("Pasta", new Item("Roland", 250, "2lb",
+                ContainerType.JAR, "Eggs", PlaceOrder.imageId_eggs,
+                "Description 1", "Description 2"));
+
+        itemMap.put("Rice", new Item("Carrington", 250, "2lb",
+                ContainerType.JAR, "Butter", PlaceOrder.imageId_butter,
+                "Description 1", "Description 2"));
+
+        itemMap.put("Salt", new Item("Florida Crystals", 250, "2lb",
+                ContainerType.JAR, "Cane Sugar", PlaceOrder.imageId_cane_sugar,
+                "Description 1", "Description 2"));
+
+        itemMap.put("Sugar", new Item("Velvetta", 250, "2lb",
+                ContainerType.JAR, "Cheese", PlaceOrder.imageId_cheese,
+                "Description 1", "Description 2"));
+
+        itemMap.put("Wheat Bread", new Item("Roland", 250, "2lb",
+                ContainerType.JAR, "Eggs", PlaceOrder.imageId_eggs,
+                "Description 1", "Description 2"));
+
+    }
+
+    public HashMap<String, Item> getItemMap() {
+
+        return itemMap;
+
+    }
 
 }
 
