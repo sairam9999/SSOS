1182d953a75e5363ab81953ef3544e389d48d5d9
sairam9999 <vsairamanroid@gmail.com>
2018-09-17 23:47:17 -0400

    Logic Related to onItemClickListener after adding the plus symbol

--- app/src/main/java/com/example/svankayalapati/ssos/PlaceOrder.java
+++ app/src/main/java/com/example/svankayalapati/ssos/PlaceOrder.java
@@ -1,18 +1,24 @@
 package com.example.svankayalapati.ssos;
 
+import android.app.Activity;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.svankayalapati.ssos.adapter.ItemRowAdapter;
 import com.example.svankayalapati.ssos.db.Item;
 import com.example.svankayalapati.ssos.db.ItemDetails;
+import com.google.android.gms.common.util.Strings;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -26,6 +32,7 @@ TextView itemDescription_textview;
 Button button_itemImage;
 TextView textview_itemdescription1;
 TextView textview_itemdescription2;
+Context applicationContext;
 
 public static int imageId_butter;
     public static int imageId_cane_sugar;
@@ -36,7 +43,7 @@ public static int imageId_butter;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_place_order);
-
+applicationContext = this;
 
         itemDescription_textview = (TextView) findViewById(R.id.itemsDescription_textview);
         button_itemImage = (Button) findViewById(R.id.button_itemImage);
@@ -48,6 +55,22 @@ public static int imageId_butter;
         textview_itemdescription2 = (TextView) findViewById(R.id.textview_itemdescription2);
 
         itemDetails.init();
+
+        /*
+
+        mListview = (ListView) findViewById(R.id.listSchool);
+
+        // Set some data to array list
+        mArrData = new ArrayList<String>(Arrays.asList("111,222,333,444,555,666".split(",")));
+
+        // Initialize adapter and set adapter to list view
+        mAdapter = new SchoolAdapter(ListviewActivity.this, mArrData);
+        mListview.setAdapter(mAdapter);
+        mAdapter.notifyDataSetChanged();
+        */
+
+
+
         final ListView listview = (ListView) findViewById(R.id.place_order_listview);
         String[] values = new String[]{"Butter", "Cane Sugar", "Cheese", "Eggs", "Milk",
                 "Nescafe", "Oil", "Pasta", "Rice", "Salt", "Sugar", "Wheat Bread",
@@ -57,6 +80,10 @@ public static int imageId_butter;
         for (int i = 0; i < values.length; ++i) {
             list.add(values[i]);
         }
+       // ItemRowAdapter itemRowAdapter = new ItemRowAdapter(this, list);
+       // listview.setAdapter(itemRowAdapter);
+       // itemRowAdapter.notifyDataSetChanged();
+
         final StableArrayAdapter adapter = new StableArrayAdapter(this,
                 android.R.layout.simple_list_item_1, list);
         listview.setAdapter(adapter);
@@ -91,15 +118,43 @@ public static int imageId_butter;
     class StableArrayAdapter extends ArrayAdapter<String> {
 
         HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
-
-        public StableArrayAdapter(Context context, int textViewResourceId,
+        private Activity mContext;
+        private List<String> mArrSchoolData;
+        public StableArrayAdapter(Activity context, int textViewResourceId,
                                   List<String> objects) {
             super(context, textViewResourceId, objects);
+            mContext = context;
+            mArrSchoolData = objects;
             for (int i = 0; i < objects.size(); ++i) {
                 mIdMap.put(objects.get(i), i);
             }
         }
+        public View getView(int position, View view, ViewGroup parent){
+           // LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            LayoutInflater inflater = mContext.getLayoutInflater();
+            view = inflater.inflate(R.layout.layout_itemrow, null, true);
 
+
+            // get the reference of textView and button
+            TextView txtSchoolTitle = (TextView) view.findViewById(R.id.txtSchoolTitle);
+            Button btnAction = (Button) view.findViewById(R.id.btnAction);
+
+            // Set the title and button name
+            txtSchoolTitle.setText(mArrSchoolData.get(position));
+            btnAction.setBackgroundResource(R.drawable.icon_list_cart);
+
+            // Click listener of button
+//            btnAction.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    // Logic goes here
+//
+//                    Toast.makeText(applicationContext, "Place an Order" +view.getId(), Toast.LENGTH_LONG).show();
+//                }
+//            });
+
+            return view;
+                 }
         @Override
         public long getItemId(int position) {
             String item = getItem(position);
